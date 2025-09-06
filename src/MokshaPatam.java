b1314f1adccfb95c249d975780816ff427c8ce8f
williambeesley <wpbeesley@gmail.com>
2025-09-06 10:17:20 -0700

    v2

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-
+import java.util.*;
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -17,13 +16,26 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        int moves = 0;
-        int square = 1;
-        // sort the list to make the closest ladders first
-        Arrays.sort(ladders);
-        // while loop so that we keep repeating the code until we get to the end of the board
-        while (square <= boardsize) {
+        // I will also make a dictionary for the chutes and ladders to make it easier to index later
+        Map<Integer, Integer> chutes_ladders = new HashMap<>();
+        for (int i = 0; i < ladders.length; i++) {
+            chutes_ladders.put(ladders[i][0], ladders[i][1]);
+        }
+        for (int i = 0; i < snakes.length; i++) {
+            chutes_ladders.put(snakes[i][0], ladders[i][1]);
+        }
+        // first I need to make the graph
+        Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        // now we add the edges by going through each point
+        for (int i = 0; i < boardsize; i++) {
+            // add the vertex first
+            graph.putIfAbsent(i, new ArrayList<>());
+            // now go through the next 6 squares and add them to the graph;
+            for (int j = 0; j < 6; j++) {
+                // check if any of the squares are chutes or ladders
 
+                // if not we can just add them directly to the graph
+            }
         }
         return 0;
     }
